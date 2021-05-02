0036198264f654a425a6fc30a7548e45514d2ca0
Byytelope <shadhanm@gmail.com>
2021-05-03 02:55:15 +0500

    Use custom btn 404 | fix 404 text sizes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { useLocation } from "wouter";
+import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
 
+import CustomButton from "../components/CustomButton";
 import Header from "../components/Header";
 
 export default function FourOFour() {
@@ -25,10 +27,25 @@ export default function FourOFour() {
         animate: { opacity: 1, y: 0 },
     };
 
-    const Button = () => {
-        return (
+    return (
+        <div className="h-screen flex flex-col dark:bg-tch-dark-surface">
+            <Header delay={0.5} />
+            <motion.div variants={animContainer} initial="initial" animate="animate">
+                <motion.p
+                    className="font-header text-3xl lg:text-4xl xl:text-5xl text-tch-gray-dk dark:text-tch-gray-md text-center mt-auto px-4 pb-4"
+                    variants={animLines}
+                >
+                    {randomResponse}
+                </motion.p>
+                <motion.p
+                    className="font-normal text-xl md:text-2xl text-tch-gray-dk dark:text-tch-gray-md text-center px-4 pb-4"
+                    variants={animLines}
+                >
+                    The page <b className="text-red-500">{location}</b> doesn&apos;t exist.
+                </motion.p>
+            </motion.div>
             <motion.div
-                className="py-4"
+                className="py-4 m-auto"
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 10 }}
@@ -38,39 +55,11 @@ export default function FourOFour() {
                     delay: 2 / 10,
                 }}
             >
-                <motion.div
-                    className="w-full-sm rounded-lg bg-tch-gray-lt dark:bg-tch-dark-surface-2 hover:bg-tch-gray-md dark:hover:bg-tch-gray-dk hover:shadow-lg text-center text-tch-gray-dk dark:text-tch-gray-md text-1.5xl font-header cursor-pointer transition-shadow duration-500 ease-out py-4 px-6"
+                <CustomButton
+                    btnData={{ text: "BACK TO HOME", icon: faLongArrowAltLeft }}
                     onClick={() => setLocation("/")}
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                    transition={{ type: "spring", bounce: 0.5 }}
-                >
-                    <p>BACK TO HOME</p>
-                </motion.div>
+                />
             </motion.div>
-        );
-    };
-
-    return (
-        <div className="h-screen flex flex-col dark:bg-tch-dark-surface">
-            <Header delay={0.5} />
-            <motion.div variants={animContainer} initial="initial" animate="animate">
-                <motion.p
-                    className="font-header text-3xl md:text-5xl lg:text-6xl xl:text-6xl text-tch-gray-dk dark:text-tch-gray-md text-center mt-auto px-4 pb-4"
-                    variants={animLines}
-                >
-                    {randomResponse}
-                </motion.p>
-                <motion.p
-                    className="font-normal text-xl md:text-2xl lg:text-2xl xl:text-3xl text-tch-gray-dk dark:text-tch-gray-md text-center px-4 pb-4"
-                    variants={animLines}
-                >
-                    The page <b className="text-red-500">{location}</b> doesn&apos;t exist.
-                </motion.p>
-            </motion.div>
-            <div className="m-auto">
-                <Button />
-            </div>
         </div>
     );
 }
